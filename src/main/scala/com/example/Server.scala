0402a3747d77a25aba9c93dad5148c81779c7f45
Rahil Bohra <rahil@bohra.co.uk>
2015-02-11 00:19:51 +0000

    = fix build

--- src/main/scala/com/example/Server.scala
+++ src/main/scala/com/example/Server.scala
@@ -1,17 +1,11 @@
 package com.example
 
-import com.twitter.finagle.{Http, Service}
-import com.twitter.util.{Await, Future}
-import com.twitter.finagle.http.Response
-import java.net.InetSocketAddress
-import org.jboss.netty.handler.codec.http._
+import akka.actor.ActorSystem
 import spray.routing.SimpleRoutingApp
 import util.Properties
-import java.net.URI
-import java.sql.Connection
-import java.sql.DriverManager
 
 object Server extends SimpleRoutingApp with App {
+  implicit val system = ActorSystem("goodnews-system")
   val port = Integer.parseInt(Properties.envOrElse("PORT", "8080"))
   startServer(interface = "0.0.0.0", port = port) {
     path("ping") {
